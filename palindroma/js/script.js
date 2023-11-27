baa1982d603fa6f27600781527c94cb51cf6dda5
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-27 15:10:52 +0100

    config: addeventlistener js

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,32 +3,40 @@
 
 
 // Chiedo all'utente di inserire un nome
-// let userWord = prompt('Inserisci una parola');
+ let userWord = document.getElementById('input');
+ let sendButton = document.getElementById('button');
+ 
 
 // creo la funzione in cui stabilisco se la parola è polindroma 
+sendButton.addEventListener ('click', function () {
 
-function wordPalindroma(word) {
+    function wordPalindroma(word) {
 
-    // inverto la parola scritta 
-
-    let wordReverse = word.split('').reverse().join('');
-
-    // stabilisco le opzioni
-    let risultato;
-
-    if (userWord === wordReverse){
-        risultato = "la parola è palindroma"
-
-    } else{
-
-        risultato = "la parola non è palindroma"
+        // inverto la parola scritta 
+    
+        let wordReverse = word.split('').reverse().join('');
+    
+        // stabilisco le opzioni
+        let risultato = document.getElementById('risultato');
+    
+        if (word === wordReverse){
+            risultato.innerHTML = "La parola è palindroma"
+    
+        } else{
+    
+            risultato.innerHTML = "La parola non è palindroma"
+        }
+        
+        return risultato;
     }
     
-    return risultato;
-}
+    
+    
+    wordPalindroma (userWord.value);
+
+    console.log(wordPalindroma(userWord.value));
+    
+});
 
 
-
-wordPalindroma (userWord);
-
-// console.log(wordPalindroma(userWord));
+ 
